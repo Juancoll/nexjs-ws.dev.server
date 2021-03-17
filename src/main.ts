17b9aac46b83a840c86c208eaad228dc0e6f3813
Juancoll <juan.coll@nex-group.io>
2021-03-17 21:36:51 +0100

    update to nexjs-ws 2.0.0 y socket.io 4.0.0

--- src/main.ts
+++ src/main.ts
@@ -12,16 +12,31 @@ import { AuthStrategy } from './auth/auth.strategy';
 import { AuthContract } from './contracts/auth.contract';
 import *  as tools from './tools';
 
-
 const app = Express();
 app.use(Express.static(path.resolve(__dirname + '/../public')));
 const http = Http.createServer(app);
 const ioServer = new io.Server(http, {
-    path: "/",
+    // [ default path is /socket.io/ - we can test with another - uncomment next line ]
+    // path: "/wsapi/",
+    // explicit cors 
     cors: {
         origin: "*"
-    }
+    },
 });
+
+// [ debug underlying engineIO protocol ]
+// (ioServer as any).engine.on('connection', (socket: any) => {
+//     console.log('[engine.io][connection]')
+
+//     // socket.on('message', (data: any) => console.log('[engine.io][message]', JSON.stringify(data)));
+//     socket.on('close', (data: any) => console.log('[engine.io][close]', JSON.stringify(data)));
+//     socket.on('error', (data: any) => console.log('[engine.io][error]', JSON.stringify(data)));
+//     socket.on('flush', (data: any) => console.log('[engine.io][flush]', JSON.stringify(data)));
+//     socket.on('drain', (data: any) => console.log('[engine.io][drain]', JSON.stringify(data)));
+//     socket.on('packet', (data: any) => console.log('[engine.io][packet]', JSON.stringify(data)));
+//     socket.on('packetCreate', (data: any) => console.log('[engine.io][packetCreate]', JSON.stringify(data)));
+// })
+
 
 app.use(cors({
     origin: true,
@@ -53,6 +68,15 @@ ioServer.on('connection', (socket: io.Socket) => {
         console.log(`[socket.io] remove client: ${socket.id}`);
     });
 });
+
+ioServer.of("/admin").on('connection', (socket: io.Socket) => {
+    console.log(`[socket.io][nsp: /admin] add    client: ${socket.id}`);
+    socket.on('disconnect', () => {
+        console.log(`[socket.io][nsp: /admin]  remove client: ${socket.id}`);
+    });
+});
+
+
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
